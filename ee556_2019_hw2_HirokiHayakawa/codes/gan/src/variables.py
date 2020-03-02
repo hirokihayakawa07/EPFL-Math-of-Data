8b5807ea1089213c8d9c2535f5be92b4bd694fbb
hirokihayakawa07 <55525471+hirokihayakawa07@users.noreply.github.com>
2020-03-02 14:53:31 +0100

    Add files via upload

---
+++ ee556_2019_hw2_HirokiHayakawa/codes/gan/src/variables.py
@@ -0,0 +1,38 @@
+import torch
+
+from torch import nn
+
+
+class LinearGenerator(nn.Module):
+    def __init__(self, noise_dim=2, output_dim=2):
+        super().__init__()
+        self.W = nn.Parameter(torch.randn(noise_dim, output_dim))
+        self.b = nn.Parameter(2 * torch.randn(output_dim))
+
+    def forward(self, z):
+        """
+        Evaluate on a sample. The variable z contains one sample per row
+        """
+        # TODO
+        output = z @ self.W + self.b
+        return output
+
+class LinearDualVariable(nn.Module):
+    def __init__(self, input_dim=2):
+        super().__init__()
+        self.v = nn.Parameter(torch.randn(input_dim))
+
+    def forward(self, x):
+        """
+        Evaluate on a sample. The variable x contains one sample per row
+        """
+        # TODO
+        output = x @ self.v
+        return output
+
+    def enforce_lipschitz(self):
+        """Enforce the 1-Lipschitz condition of the function"""
+        with torch.no_grad():
+            # TODO
+            output = self.v / torch.norm(self.v)
+            return output
